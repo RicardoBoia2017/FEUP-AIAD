215182bd8271aeb6e3a9903c509f6f9d5c2da41e
Ricardo Boia <up201505244@fe.up.pt>
2019-10-02 16:45:37 +0100

    Estabilished connection between Passenger and Bus

--- src/BusAgent.java
+++ src/BusAgent.java
@@ -1,9 +1,60 @@
-public class BusAgent {
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+
+import java.util.ArrayList;
+
+public class BusAgent extends Agent{
 
     private Coordinates coords;
+    private ArrayList<Integer> itinerary = new ArrayList<>();
 
-    BusAgent()
-    {
-        coords = new Coordinates(0,0);
+    protected void setup() {
+        this.coords = new Coordinates(0,0); //TODO define starting point
+        System.out.println("Bus started");
+
+        // Add a TickerBehaviour that schedules a request to seller agents every minute
+        addBehaviour(new TickerBehaviour(this, 10000) {
+            protected void onTick() {
+                System.out.println("Checking new passengers");
+                // Update the list of seller agents
+                DFAgentDescription template = new DFAgentDescription();
+                ServiceDescription sd = new ServiceDescription();
+                sd.setType("bus-agency"); //TODO define type of service
+                template.addServices(sd);
+                try {
+                    DFAgentDescription[] result = DFService.search(myAgent, template);
+                    System.out.println("Found the following customers:");
+                    AID[] customers = new AID[result.length];
+
+                    for (int i = 0; i < result.length; ++i) {
+                        customers[i] = result[i].getName();
+                        System.out.println(customers[i].getName());
+                    }
+
+                    System.out.println("");
+                }
+                catch (FIPAException fe) {
+                    fe.printStackTrace();
+                }
+            }
+        } );
+
     }
+
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        }
+        catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        System.out.println("Bus stopped operating");
+    }
+
 }
--- src/PassengerAgent.java
+++ src/PassengerAgent.java
@@ -1,7 +1,45 @@
-public class PassengerAgent {
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 
-    PassengerAgent ()
-    {
-        
+public class PassengerAgent extends Agent {
+
+    protected void setup() {
+        Object[] args = getArguments();
+        if (args != null && args.length == 2) {
+            int startStop, endStop;
+
+            startStop = Integer.parseInt((String)  args[0]);
+            endStop = Integer.parseInt((String)  args[1]);
+
+            System.out.println("Client Name: " + getLocalName());
+            System.out.println("Current stop: " + startStop);
+            System.out.println("Ending stop: " + endStop + "\n");
+
+            // Register the client in the yellow pages
+            DFAgentDescription dfd = new DFAgentDescription();
+            dfd.setName(getAID());
+            ServiceDescription sd = new ServiceDescription();
+            sd.setType("bus-agency"); //TODO define type of service
+            sd.setName("JADE-bus-agency");
+            dfd.addServices(sd);
+            try {
+                DFService.register(this, dfd);
+            }
+            catch (FIPAException fe) {
+                fe.printStackTrace();
+            }
+
+        } else {
+            // Make the agent terminate
+            System.out.println("No starting stop and/or ending stop specified");
+            doDelete();
+        }
+    }
+
+    protected void takeDown() {
+        System.out.println("Passenger reached his destination");
     }
 }
