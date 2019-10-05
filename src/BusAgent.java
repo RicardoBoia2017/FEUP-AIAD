0e6920840796d39941f80cd868e69d0d8b60bc74
Ricardo Boia <up201505244@fe.up.pt>
2019-10-05 15:20:27 +0100

    Passenger is choosing bus that gives him shorter time (random value)

--- src/BusAgent.java
+++ src/BusAgent.java
@@ -1,8 +1,11 @@
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 import java.util.ArrayList;
 
@@ -28,6 +31,8 @@ public class BusAgent extends Agent{
             fe.printStackTrace();
         }
 
+        addBehaviour(new OfferRequestsServer());
+
     }
 
     protected void takeDown() {
@@ -40,5 +45,43 @@ public class BusAgent extends Agent{
 
         System.out.println("Bus stopped operating");
     }
+
+    /**
+     Inner class OfferRequestsServer.
+     This is the behaviour used by Book-seller agents to serve incoming requests
+     for offer from buyer agents.
+     If the requested book is in the local catalogue the seller agent replies
+     with a PROPOSE message specifying the price. Otherwise a REFUSE message is
+     sent back.
+     */
+    private class OfferRequestsServer extends CyclicBehaviour {
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                // CFP Message received. Process it
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+
+                Integer price = (int)(Math.random() * 30);
+                System.out.println(getLocalName() + " " + price);
+                if (price != null) {
+                    // The requested book is available for sale. Reply with the price
+                    reply.setPerformative(ACLMessage.PROPOSE);
+                    reply.setContent(String.valueOf(price));
+                }
+                else {
+                    // The requested book is NOT available for sale.
+                    reply.setPerformative(ACLMessage.REFUSE);
+                    reply.setContent("not-available");
+                }
+                System.out.println(reply);
+                myAgent.send(reply);
+            }
+            else {
+                block();
+            }
+        }
+    }  // End of inner class OfferRequestsServer
 
 }
--- src/PassengerAgent.java
+++ src/PassengerAgent.java
@@ -1,18 +1,24 @@
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 public class PassengerAgent extends Agent {
 
+    private AID[] targetBuses;
+    private int startStop;
+    private int endStop;
+
     protected void setup() {
         Object[] args = getArguments();
-        if (args != null && args.length == 2) {
-            int startStop, endStop;
 
+        if (args != null && args.length == 2) {
             startStop = Integer.parseInt((String)  args[0]);
             endStop = Integer.parseInt((String)  args[1]);
 
@@ -21,7 +27,7 @@ public class PassengerAgent extends Agent {
             System.out.println("Ending stop: " + endStop + "\n");
 
             // Add a TickerBehaviour that checks for bus
-            addBehaviour(new TickerBehaviour(this, 10000) {
+            addBehaviour(new TickerBehaviour(this, 5000) {
                 protected void onTick() {
 
                     DFAgentDescription template = new DFAgentDescription();
@@ -31,18 +37,23 @@ public class PassengerAgent extends Agent {
 
                     try {
                         DFAgentDescription[] result = DFService.search(myAgent, template);
-                        System.out.println("Found the following bus:");
+                        System.out.println("Found the following buses:");
 
-                        AID [] sellerAgents = new AID[result.length];
+                        targetBuses = new AID[result.length];
 
                         for (int i = 0; i < result.length; ++i) {
-                            sellerAgents[i] = result[i].getName();
-                            System.out.println(sellerAgents[i].getName());
+                            targetBuses[i] = result[i].getName();
+                            System.out.println(targetBuses[i].getName());
                         }
+
+                        System.out.println("");
                     }
                     catch (FIPAException fe) {
                         fe.printStackTrace();
                     }
+
+                    // Perform the request
+                    myAgent.addBehaviour(new RequestPerformer());
                 }
             } );
 
@@ -58,9 +69,109 @@ public class PassengerAgent extends Agent {
             DFService.deregister(this);
         }
         catch (FIPAException fe) {
-            fe.printStackTrace();
         }
 
         System.out.println("Passenger reached his destination");
     }
+
+    /**
+     Inner class RequestPerformer.
+     This is the behaviour used by Book-buyer agents to request seller
+     agents the target book.
+     */
+    private class RequestPerformer extends Behaviour {
+        private AID bestBus; // The agent who provides the best offer
+        private int bestTime;  // The best offered time
+        private int repliesCnt = 0; // The counter of replies from bus agents
+        private MessageTemplate mt; // The template to receive replies
+        private int step = 0;
+
+        public void action() {
+            switch (step) {
+                case 0:
+                    // Send the cfp to all sellers
+                    ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+                    for (int i = 0; i < targetBuses.length; ++i) {
+                        cfp.addReceiver(targetBuses[i]);
+                    }
+                    cfp.setContent(String.valueOf(startStop));
+                    cfp.setConversationId("bus-agency");
+                    cfp.setReplyWith("cfp"+System.currentTimeMillis()); // Unique value
+                    myAgent.send(cfp);
+                    // Prepare the template to get proposals
+                    mt = MessageTemplate.and(MessageTemplate.MatchConversationId("bus-agency"),
+                            MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+                    step = 1;
+                    System.out.println("Sending message");
+                    break;
+                case 1:
+                    // Receive all proposals/refusals from seller agents
+                    ACLMessage reply = myAgent.receive(mt);
+                    if (reply != null) {
+                        // Reply received
+                        if (reply.getPerformative() == ACLMessage.PROPOSE) {
+                            // This is an offer
+                            int time = Integer.parseInt(reply.getContent());
+                            if (bestBus == null || time < bestTime) {
+                                // This is the best offer at present
+                                bestTime = time;
+                                bestBus = reply.getSender();
+                            }
+                        }
+                        repliesCnt++;
+                        if (repliesCnt >= targetBuses.length) {
+                            // We received all replies
+                            step = 2;
+                        }
+                    }
+                    else {
+                        block();
+                    }
+                    break;
+                case 2:
+                    // Send the purchase order to the seller that provided the best offer
+                    ACLMessage order = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+                    order.addReceiver(bestBus);
+                    order.setContent(String.valueOf(startStop));
+                    order.setConversationId("bus-agency");
+                    order.setReplyWith("order"+System.currentTimeMillis());
+                    myAgent.send(order);
+                    // Prepare the template to get the purchase order reply
+                    mt = MessageTemplate.and(MessageTemplate.MatchConversationId("bus-agency"),
+                            MessageTemplate.MatchInReplyTo(order.getReplyWith()));
+                    step = 3;
+                    break;
+                case 3:
+                    // Receive the purchase order reply
+                    reply = myAgent.receive(mt);
+                    if (reply != null) {
+                        // Purchase order reply received
+                        if (reply.getPerformative() == ACLMessage.INFORM) {
+                            // Purchase successful. We can terminate
+                            System.out.println("Bus \" " + reply.getSender().getName() + " \" will arrive in the next few minutes");
+                            System.out.println("Time = " + bestTime);
+                            myAgent.doDelete();
+                        }
+                        else {
+                            System.out.println("Attempt failed: requested book already sold.");
+                        }
+
+                        step = 4;
+                    }
+                    else {
+                        block();
+                    }
+                    System.out.println("Bus: " + bestBus);
+                    System.out.println("Time: " + bestTime);
+                    break;
+            }
+        }
+
+        public boolean done() {
+            if (step == 2 && bestBus == null) {
+                System.out.println("There are no buses");
+            }
+            return ((step == 2 && bestBus == null) || step == 4);
+        }
+    }  // End of inner class RequestPerformer
 }
