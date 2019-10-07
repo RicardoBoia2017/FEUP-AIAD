dc64ece8a2fb4a4960414fd88c2e17e40ed8c766
Tiago Ribeiro <tiago7b27@gmail.com>
2019-10-08 00:35:05 +0100

    main class file
    agent generation

---
+++ src/JADELauncher.java
@@ -0,0 +1,47 @@
+import jade.core.Agent;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+
+public class JADELauncher {
+
+	public static void main(String[] args) {
+		Runtime rt = Runtime.instance();
+
+		Profile p1 = new ProfileImpl();
+		//p1.setParameter(...);
+		ContainerController mainContainer = rt.createMainContainer(p1);
+
+		AgentController ac1;
+		try {
+                        Object[] stops = {"1","2"};
+			ac1 = mainContainer.createNewAgent("pass1", "PassengerAgent", stops);
+			ac1.start();
+		} catch (StaleProxyException e) {
+                    e.printStackTrace();
+		}
+
+                
+                AgentController ac2;
+		try {
+                        Object[] stops = {1,2};
+			ac2 = mainContainer.acceptNewAgent("bus1", new BusAgent());
+			ac2.start();
+		} catch (StaleProxyException e) {
+                    e.printStackTrace();
+		}
+
+                
+		AgentController ac3;
+		try {
+			ac3 = mainContainer.acceptNewAgent("myRMA", new jade.tools.rma.rma());
+			ac3.start();
+		} catch (StaleProxyException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
